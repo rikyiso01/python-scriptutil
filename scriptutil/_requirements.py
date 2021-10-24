9697cfc3dbfddea86219c3509605bcb5a9603731
rikyiso01 <31405152+RikyIsola@users.noreply.github.com>
2021-10-24 11:13:20 +0200

    add requirements docstring

--- scriptutil/_requirements.py
+++ scriptutil/_requirements.py
@@ -6,6 +6,7 @@ from shutil import copy
 
 
 def requirements(*requirements: str) -> NoReturn:
+    """Check if the program satisfies the given requirements"""
     run([executable, "-m", "pip", "install", *requirements], check=True)
     execv(executable, [executable] + argv)
 
